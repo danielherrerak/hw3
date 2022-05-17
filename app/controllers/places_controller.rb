363d911e097b466d7bf7f73ac7f041902c6f8cac
danielherrerak <daniel.herrera@kellogg.northwestern.edu>
2022-05-17 23:00:30 +0000

    test

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,10 +4,6 @@ def index
     @places = Place.all
 end
 
-def show
-    @place = Place.find_by({"id" => params["id"]})
-    @posts = Post.where({"place_id" => @place["id"]})
-end
 
 def new
     @place = Place.new
